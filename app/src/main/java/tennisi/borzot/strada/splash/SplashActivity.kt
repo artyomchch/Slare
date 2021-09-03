e5f0bf48b574b8ffa9d26bf7efe4e9b503a506d7
Artyom Kozlov <artyom.ch.ch@gmail.com>
2021-09-03 22:01:13 +0300

    i think, speedometer ready, on/off

--- app/src/main/java/tennisi/borzot/strada/splash/SplashActivity.kt
+++ app/src/main/java/tennisi/borzot/strada/splash/SplashActivity.kt
@@ -1,39 +1,16 @@
 package tennisi.borzot.strada.splash
 
 import android.content.Intent
-import android.os.Build
-import android.os.Bundle
-import android.os.Handler
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import tennisi.borzot.strada.R
 import tennisi.borzot.strada.onboarding.MVPOnBoarding.OnBoardingMain
 
-@Suppress("DEPRECATION")
-class SplashActivity : AppCompatActivity(), SplashInterface.View {
-
-    private var presenter: SplashPresenter? = null
-
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+       // setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        presenter = SplashPresenter(this)
-        presenter!!.hideUI(window)
-        presenter!!.restorePrefData(this, intent)
-
+        val intent = Intent(this, OnBoardingMain::class.java)
+        this.startActivity(intent)
     }
-
-
-    override fun initView() {
-
-    }
-
-    override fun animation() {
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        finish()
-    }
-
-
-
 }
