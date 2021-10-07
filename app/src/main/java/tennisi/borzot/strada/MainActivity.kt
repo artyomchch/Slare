1c0c79ee19ea378fe6bdb6ceefbc4ea265482aaf
Artyom Kozlov <artyom.ch.ch@gmail.com>
2021-10-07 17:09:31 +0300

    refactor main activity

--- app/src/main/java/tennisi/borzot/strada/MainActivity.kt
+++ app/src/main/java/tennisi/borzot/strada/MainActivity.kt
@@ -3,8 +3,6 @@ package tennisi.borzot.strada
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import tennisi.borzot.strada.databinding.ActivityMainBinding
@@ -16,7 +14,6 @@ import tennisi.borzot.strada.fragments.speed.SpeedFragment
 
 class MainActivity : AppCompatActivity() {
 
-    //   private lateinit var mScreenSlidePagerAdapter: ScreenSlidePagerAdapter
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val addFragment = AddFragment()
     private val equalizerFragment = EqualizerFragment()
@@ -26,22 +23,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        authentication()
         replaceFragment(speedFragment)
+        selectedListener()
         binding.bottomNavigationMenu.menu.findItem(R.id.speedFragment).isChecked = true
-        binding.mainFragmentToolbar.toolbarText.text = getString(R.string.speed)
-        binding.mainFragmentToolbar.toolbarImageItem.setImageResource(R.drawable.ic_baseline_speed_purple)
+        setResource(getString(R.string.speed), R.drawable.ic_baseline_speed_purple)
+    }
 
+
+    private fun selectedListener() {
         binding.bottomNavigationMenu.setOnItemSelectedListener {
             changingTabs(it.itemId)
             true
         }
+    }
 
-
-
+    private fun authentication() {
         auth = FirebaseAuth.getInstance()
         val currentUser = auth.currentUser
 
@@ -49,131 +49,42 @@ class MainActivity : AppCompatActivity() {
             .load(currentUser?.photoUrl)
             .circleCrop()
             .into(binding.mainFragmentToolbar.toolbarImageSignIn)
-
-
-        //var adapter = ScreenSlidePagerAdapter(supportFragmentManager, lifecycle)
-
-        //mPagerAdapter = PagerViewAdapter(supportFragmentManager)
-
-        //mScreenSlidePagerAdapter = ScreenSlidePagerAdapter(this)
-        //binding.mViewPager.adapter = mScreenSlidePagerAdapter
-        //  binding.mViewPager.offscreenPageLimit = 5
-
-
-
-        // add page change listener
-//        binding.mViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-//            override fun onPageSelected(position: Int) {
-//                super.onPageSelected(position)
-//                changingTabs(position)
-//            }
-//
-//
-//        })
-
-
-//        binding.layoutAddCar.setOnClickListener {
-//            binding.mViewPager.setCurrentItem(0, true)
-//        }
-//
-//
-//
-//        binding.layoutEqualizer.setOnClickListener {
-//            binding.mViewPager.setCurrentItem(1, true)
-//        }
-//
-//
-//        binding.layoutSpeed.setOnClickListener {
-//            binding.mViewPager.setCurrentItem(2, true)
-//        }
-//
-//
-//        binding.layoutPlayer.setOnClickListener {
-//            binding.mViewPager.setCurrentItem(3, true)
-//        }
-//
-//
-//
-//        binding.layoutSettings.setOnClickListener {
-//            binding.mViewPager.setCurrentItem(4, true)
-//        }
-//
-//
-//        //default tab
-//   //     binding.mViewPager.currentItem = 2
-//        binding.speedBtn.setImageResource(R.drawable.ic_baseline_speed_purple)
-//
-
     }
 
-
-        //binding.bottomNavigationMenu.setupWithNavController(binding.navigationHost.findNavController())
-
-//        binding.bottomNavigationMenu.setOnItemSelectedListener {
-//
-//            changingTabs(it.itemId)
-//            true
-//        }
-
-
-
-    private fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
         transaction.commit()
     }
 
+    private fun changingTabs(idFragment: Int) {
 
-    private fun changingTabs(position: Int) {
-        if (position == R.id.addFragment) {
-            binding.mainFragmentToolbar.toolbarText.text = getString(R.string.vehicle)
-            binding.mainFragmentToolbar.toolbarImageItem.setImageResource(R.drawable.ic_baseline_add_purple)
+        if (idFragment == R.id.addFragment) {
+            setResource(getString(R.string.vehicle), R.drawable.ic_baseline_add_purple)
             replaceFragment(addFragment)
-//            binding.addBtn.setImageResource(R.drawable.ic_baseline_add_purple)
-//            binding.equBtn.setImageResource(R.drawable.ic_baseline_equalizer_black)
-//            binding.speedBtn.setImageResource(R.drawable.ic_baseline_speed_black)
-//            binding.newsBtn.setImageResource(R.drawable.ic_baseline_rss_feed_24)
-//            binding.setBtn.setImageResource(R.drawable.ic_baseline_settings_black)
         }
-        if (position == R.id.equalizerFragment) {
-            binding.mainFragmentToolbar.toolbarText.text = getString(R.string.equalizer)
-            binding.mainFragmentToolbar.toolbarImageItem.setImageResource(R.drawable.ic_baseline_equalizer_purple)
+        if (idFragment == R.id.equalizerFragment) {
+            setResource(getString(R.string.equalizer), R.drawable.ic_baseline_equalizer_purple)
             replaceFragment(equalizerFragment)
-//            binding.addBtn.setImageResource(R.drawable.ic_baseline_add_black)
-//            binding.equBtn.setImageResource(R.drawable.ic_baseline_equalizer_purple)
-//            binding.speedBtn.setImageResource(R.drawable.ic_baseline_speed_black)
-//            binding.newsBtn.setImageResource(R.drawable.ic_baseline_rss_feed_24)
-//            binding.setBtn.setImageResource(R.drawable.ic_baseline_settings_black)
         }
-        if (position == R.id.speedFragment) {
-            binding.mainFragmentToolbar.toolbarText.text = getString(R.string.speed)
-            binding.mainFragmentToolbar.toolbarImageItem.setImageResource(R.drawable.ic_baseline_speed_purple)
+        if (idFragment == R.id.speedFragment) {
+            setResource(getString(R.string.speed), R.drawable.ic_baseline_speed_purple)
             replaceFragment(speedFragment)
-//            binding.addBtn.setImageResource(R.drawable.ic_baseline_add_black)
-//            binding.equBtn.setImageResource(R.drawable.ic_baseline_equalizer_black)
-//            binding.speedBtn.setImageResource(R.drawable.ic_baseline_speed_purple)
-//            binding.newsBtn.setImageResource(R.drawable.ic_baseline_rss_feed_24)
-//            binding.setBtn.setImageResource(R.drawable.ic_baseline_settings_black)
         }
-        if (position == R.id.newsFragment) {
-            binding.mainFragmentToolbar.toolbarText.text = getString(R.string.news)
-            binding.mainFragmentToolbar.toolbarImageItem.setImageResource(R.drawable.ic_baseline_rss_feed_purple)
+        if (idFragment == R.id.newsFragment) {
+            setResource(getString(R.string.news), R.drawable.ic_baseline_rss_feed_purple)
             replaceFragment(newsFragment)
-//            binding.addBtn.setImageResource(R.drawable.ic_baseline_add_black)
-//            binding.equBtn.setImageResource(R.drawable.ic_baseline_equalizer_black)
-//            binding.speedBtn.setImageResource(R.drawable.ic_baseline_speed_black)
-//            binding.newsBtn.setImageResource(R.drawable.ic_baseline_rss_feed_purple)
-//            binding.setBtn.setImageResource(R.drawable.ic_baseline_settings_black)
         }
-        if (position == R.id.settingsFragment) {
-            binding.mainFragmentToolbar.toolbarText.text = getString(R.string.settings)
-            binding.mainFragmentToolbar.toolbarImageItem.setImageResource(R.drawable.ic_baseline_settings_purple)
+        if (idFragment == R.id.settingsFragment) {
+            setResource(getString(R.string.settings), R.drawable.ic_baseline_settings_purple)
             replaceFragment(settingsFragment)
-//            binding.addBtn.setImageResource(R.drawable.ic_baseline_add_black)
-//            binding.equBtn.setImageResource(R.drawable.ic_baseline_equalizer_black)
-//            binding.speedBtn.setImageResource(R.drawable.ic_baseline_speed_black)
-//            binding.newsBtn.setImageResource(R.drawable.ic_baseline_rss_feed_24)
-//            binding.setBtn.setImageResource(R.drawable.ic_baseline_settings_purple)
+        }
+    }
+
+    private fun setResource(title: String, imageResource: Int) {
+        with(binding) {
+            mainFragmentToolbar.toolbarText.text = title
+            mainFragmentToolbar.toolbarImageItem.setImageResource(imageResource)
         }
     }
 
