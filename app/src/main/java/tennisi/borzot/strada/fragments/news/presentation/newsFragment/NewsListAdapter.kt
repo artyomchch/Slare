fc6901d7a5272a0321437e402d8b2f0d65d8e9ff
Artyom Kozlov <artyom.ch.ch@gmail.com>
2021-12-09 18:50:00 +0300

    view binding in recycler

--- app/src/main/java/tennisi/borzot/strada/fragments/news/presentation/newsFragment/NewsListAdapter.kt
+++ app/src/main/java/tennisi/borzot/strada/fragments/news/presentation/newsFragment/NewsListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.bumptech.glide.Glide
 import tennisi.borzot.strada.R
+import tennisi.borzot.strada.databinding.ItemNewsBinding
 import tennisi.borzot.strada.network.pojo.Article
 import tennisi.borzot.strada.utils.DateUtils
 
@@ -14,33 +15,36 @@ class NewsListAdapter : ListAdapter<Article, NewsItemViewHolder>(NewsItemDiffCal
     var onNewsItemLongClickListener: ((Article) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
-        return NewsItemViewHolder(view)
+        val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: NewsItemViewHolder, position: Int) {
         val newsItem = getItem(position)
-        Glide.with(viewHolder.itemView)
-            .load(newsItem.urlToImage)
-            .centerCrop()
-            .into(viewHolder.image)
-        viewHolder.author.text = newsItem.author
-        viewHolder.publishTime.text = DateUtils.dateFormat(newsItem.publishedAt)
-        viewHolder.title.text = newsItem.title
-        viewHolder.description.text = newsItem.description
-        viewHolder.source.text = newsItem.source.name
-        (viewHolder.itemView.resources.getString(R.string.marker_dot) + " " + DateUtils.dateToTimeFormat(newsItem.publishedAt)).also {
-            viewHolder.time.text = it
+        with(viewHolder.binding){
+            Glide.with(root)
+                .load(newsItem.urlToImage)
+                .centerCrop()
+                .into(imageNews)
+            authorText.text = newsItem.author
+            publishedAt.text = DateUtils.dateFormat(newsItem.publishedAt)
+            titleNews.text = newsItem.title
+            descNews.text = newsItem.description
+            sourceNews.text = newsItem.source.name
+            (viewHolder.itemView.resources.getString(R.string.marker_dot) + " " + DateUtils.dateToTimeFormat(newsItem.publishedAt)).also {
+                timeNews.text = it
+            }
+
+            root.setOnClickListener {
+                onNewsItemClickListener?.invoke(newsItem)
+            }
+
+            root.setOnLongClickListener {
+                onNewsItemLongClickListener?.invoke(newsItem)
+                true
+            }
         }
 
-        viewHolder.itemView.setOnClickListener {
-            onNewsItemClickListener?.invoke(newsItem)
-        }
-
-        viewHolder.itemView.setOnLongClickListener {
-            onNewsItemLongClickListener?.invoke(newsItem)
-            true
-        }
     }
 
 }
